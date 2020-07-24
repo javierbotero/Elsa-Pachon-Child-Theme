b7833a9bd92c1295eef96c4863b0af52cd8970e9
Javier Botero <javierbotero10271@gmail.com>
2020-07-23 23:03:55 -0500

    Add iframe in header.php

--- functions.php
+++ functions.php
@@ -8,3 +8,13 @@ function my_theme_enqueue_styles() {
   wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', array(), $theme->parent()->get('Version'));
   wp_enqueue_style( 'child-style', get_stylesheet_uri(), array($parenthandle), $theme->get('Version'));
 }
+
+// run_video
+
+// add_action('wp_body_open', 'run_video');
+
+function run_video() {
+  if (is_page(6)) {
+    echo "<h1 class='video-opening'>VIDEO TO RUN</h1>";
+  }
+}
